7f5e3343116e4868c2f38eb67e580f8f9ed628a8
crisjosil <crisjosil@hotmail.com>
2019-08-06 15:54:03 +0100

    SAR Time series of a point, a region and from a shapefile
    SAR Time series of a point, a region and from a shapefile

---
+++ Earth_Engine_scripts/EE_library.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 17:00:53 2019
+
+@author: crisj
+"""
+import ee
+ee.Initialize()
+#import time
+from datetime import datetime
+#from pprint import pprint
+#import geextract
+#from geextract import ts_extract, get_date
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#import geopandas
+#import folium
+import matplotlib.dates as mdates
+
+
+def simplify(fc):
+    """Take a feature collection, as returned by mapping a reducer to a ImageCollection,
+        and reshape it into a simpler list of dictionaries
+
+    Args:
+        fc (dict): Dictionary representation of a feature collection, as returned
+            by mapping a reducer to an ImageCollection
+
+    Returns:
+        list: A list of dictionaries.
+
+    Examples:
+        >>> fc = {u'columns': {},
+        ...       u'features': [{u'geometry': None,
+        ...                      u'id': u'LC81970292013106',
+        ...                      u'properties': {u'B1': 651.8054424353023,
+        ...                                      u'B2': 676.6018246419446},
+        ...                      u'type': u'Feature'},
+        ...                     {u'geometry': None,
+        ...                      u'id': u'LC81970292013122',
+        ...                      u'properties': {u'B1': 176.99323997958842,
+        ...                                      u'B2': 235.83196553144882},
+        ...                      u'type': u'Feature'}]}
+        >>> simplify(fc)
+    """
+    def feature2dict(f):
+        id = f['id']
+        out = f['properties']
+        out.update(id=id)
+        return out
+    out = [feature2dict(x) for x in fc['features']]
+    return out
+
+def get_S1_date(out):
+    """ Obtains the SAR image acquisition date from the product ID
+
+    Args:
+        out (List of dictionaries): Each dictionary corresponds to the sentinel1 info of an image
+
+    Returns:
+        Dates : A list of the acquisition dates
+    
+    """
+    Dates=np.zeros(len(out)).tolist()
+    VH=np.zeros(len(out)).tolist()
+    VV=np.zeros(len(out)).tolist()
+    angle=np.zeros(len(out)).tolist()
+    df=pd.DataFrame()
+    for i in range(len(out)):
+        a=out[i]['id']
+        b=a.split(sep='_')
+        b1=b[4].split(sep='T')
+        b2=b1[0]
+        Dates[i]=datetime.strptime(b2, "%Y%m%d")
+        VH[i]=out[i]['VH']
+        VV[i]=out[i]['VV']
+        angle[i]=out[i]['angle']
+    
+    
+    df['Dates']=Dates
+    df['VH']=VH
+    df['VV']=VV
+    df['angle']=angle
+    df.set_index('Dates', inplace=True)
+    return(df)
+    
+def Time_series_of_a_point(Img_collection,point,title):
+    l = Img_collection.filterBounds(point).getRegion(point, 30).getInfo()
+    out = [dict(zip(l[0], values)) for values in l[1:]]
+    df=get_S1_date(out)
+    df=df.sort_index(axis = 0)
+    
+    fig, (ax, ax1,ax2) = plt.subplots(3, 1, sharex=True,figsize=(19,9)) 
+    ax.plot(df['VH'],marker='o', markersize=4, linestyle='--', linewidth=1,color="blue",label='VH '+title)
+    ax1.plot(df['VV'],marker='o', markersize=4, linestyle='--', linewidth=1,color="red",label='VV '+title)
+    ax2.plot((df['VH']-df['VV']),marker='o', markersize=4, linestyle='--', linewidth=1,color="green",label='Ratio '+title)
+    ax.legend()
+    ax1.legend()
+    ax2.legend()
+    ax.set_ylim(-28.5,-13)
+    ax1.set_ylim(-15,-9)
+    ax2.set_ylim(-13,-2.5)
+    ejes = fig.axes #which is used to extract the axes
+    # For every axis, set the x and y major locator
+    for axi in ejes:
+        axi.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=1, interval=2, tz=None))
+    plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right',)
+    ax2.tick_params(axis='x', which='major', labelsize=9)
+    plt.tight_layout()
+    
+    return(df)
+    
+def Time_series_of_a_region(Img_collection,geometry,stats,title):
+    if stats == 'mean':
+        fun = ee.Reducer.mean()
+    elif stats == 'median':
+        fun = ee.Reducer.median()
+    elif stats == 'max':
+        fun = ee.Reducer.max()
+    elif stats == 'min':
+        fun = ee.Reducer.min()
+    elif stats == 'minMax':
+        fun = ee.Reducer.minMax()
+    else:
+        raise ValueError('Unknown spatial aggregation function. Must be one of mean, median, max, or min')
+
+    def _reduce_region(image):
+                    """Spatial aggregation function for a single image and a polygon feature"""
+                    #the reduction is specified by providing the reducer (ee.Reducer.mean()), the geometry  (a polygon coord), at the scale (30 meters)
+                    stat_dict = image.reduceRegion(fun, geometry, 30);
+                    # FEature needs to be rebuilt because the backend doesn't accept to map
+                    # functions that return dictionaries
+                    return ee.Feature(None, stat_dict)
+    fc = Img_collection.filterBounds(geometry).map(_reduce_region).getInfo()
+    out = simplify(fc)
+    df=get_S1_date(out)
+    df=df.sort_index(axis = 0)
+    
+    fig, (ax, ax1,ax2) = plt.subplots(3, 1, sharex=True,figsize=(19,9)) 
+    ax.plot(df['VH'],marker='o', markersize=4, linestyle='--', linewidth=1,color="blue",label='VH '+title)
+    ax1.plot(df['VV'],marker='o', markersize=4, linestyle='--', linewidth=1,color="red",label='VV '+title)
+    ax2.plot((df['VH']-df['VV']),marker='o', markersize=4, linestyle='--', linewidth=1,color="green",label='Ratio '+title)
+    ax.legend()
+    ax1.legend()
+    ax2.legend()
+    ax.set_ylim(-28.5,-13)
+    ax1.set_ylim(-15,-9)
+    ax2.set_ylim(-13,-2.5)
+    ejes = fig.axes #which is used to extract the axes
+    # For every axis, set the x and y major locator
+    for axi in ejes:
+        axi.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=1, interval=2, tz=None))
+    plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right',)
+    ax2.tick_params(axis='x', which='major', labelsize=9)
+    plt.tight_layout()
+    
+    return(df)    
+    
+def time_series_from_shp(Img_collection,geo_df_IDMT,ID_MT,stats,save_ts,out_path):
+    df1=pd.DataFrame()
+    for k in range(geo_df_IDMT.shape[0]):
+        x, y = geo_df_IDMT['geometry'][geo_df_IDMT.index[k]].exterior.coords.xy
+        
+        e_full=[]
+        e_full_1=[]
+        for h in range(len(geo_df_IDMT['geometry'][geo_df_IDMT.index[k]].exterior.coords)):
+            e=[]
+            e.append(x[h])
+            e.append(y[h])
+            e_full.append(e)        
+        #print(e_full)
+        e_full_1.append(e_full)
+        geom = ee.Geometry.Polygon(e_full_1)
+        geometry= geom   
+        if stats == 'mean':
+            fun = ee.Reducer.mean()
+        elif stats == 'median':
+            fun = ee.Reducer.median()
+        elif stats == 'max':
+            fun = ee.Reducer.max()
+        elif stats == 'min':
+            fun = ee.Reducer.min()
+        elif stats == 'minMax':
+            fun = ee.Reducer.minMax()
+        else:
+            raise ValueError('Unknown spatial aggregation function. Must be one of mean, median, max, or min')
+        
+        def _reduce_region(image):
+                    """Spatial aggregation function for a single image and a polygon feature"""
+                    #the reduction is specified by providing the reducer (ee.Reducer.mean()), the geometry  (ee.Geometry.Polygon[]), at the scale (30 meters)
+                    stat_dict = image.reduceRegion(fun, geometry, 30);
+                    # FEature needs to be rebuilt because the backend doesn't accept to map
+                    # functions that return dictionaries
+                    return ee.Feature(None, stat_dict)
+        fc = Img_collection.filterBounds(geometry).map(_reduce_region).getInfo()
+        out = simplify(fc)
+        df=get_S1_date(out)
+        df=df.sort_index(axis = 0)
+        df['Ratio']=df['VH']-df['VV']
+        new_col_name_VH='VH_'+str(geo_df_IDMT['ID_MTL'][geo_df_IDMT.index[k]])
+        new_col_name_VV='VV_'+str(geo_df_IDMT['ID_MTL'][geo_df_IDMT.index[k]])
+        new_col_name_ratio='Ratio_'+str(geo_df_IDMT['ID_MTL'][geo_df_IDMT.index[k]])
+        df.rename(columns={'VH':new_col_name_VH,'VV':new_col_name_VV,'Ratio':new_col_name_ratio},inplace=True)
+        df1=pd.concat([df1, df], axis=1) 
+        
+    # --------------------------------------- plot each lote individually --------------------------------------    
+    #    fig, (ax, ax1,ax2) = plt.subplots(3, 1, sharex=True,figsize=(19,9)) 
+    #    ax.plot(df[new_col_name_VH],marker='o', markersize=4, linestyle='--', linewidth=1,color="blue",label=new_col_name_VH)
+    #    ax1.plot(df[new_col_name_VV],marker='o', markersize=4, linestyle='--', linewidth=1,color="red",label=new_col_name_VV)
+    #    ax2.plot((df[new_col_name_VH]-df[new_col_name_VV]),marker='o', markersize=4, linestyle='--', linewidth=1,color="green",label="Ratio "+str(geo_df_IDMT['ID_MTL'][geo_df_IDMT.index[k]]))
+    #    ax.legend()
+    #    ax1.legend()
+    #    ax2.legend()
+    #    ax.set_ylim(-28.5,-13)
+    #    ax1.set_ylim(-15,-9)
+    #    ax2.set_ylim(-13,-2.5)
+    #    ejes = fig.axes #which is used to extract the axes
+    #    # For every axis, set the x and y major locator
+    #    for axi in ejes:
+    #        axi.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=1, interval=2, tz=None))
+    #    plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right',)
+    #    ax2.tick_params(axis='x', which='major', labelsize=9)
+    #    plt.tight_layout()
+    a=df1.filter(like="VH")
+    b=df1.filter(like="VV")
+    c=df1.filter(like="Ratio")
+    fig, (ax, ax1, ax2) = plt.subplots(3, 1, sharex=True,figsize=(19,9))                        
+    # Plot three dataframes in the same chart, assigning the same colors to the columns
+    a.plot(ax=ax,  marker='o', figsize=(19, 9), markersize=4, linestyle='--', linewidth=1,color=["blue","red","green","black","magenta","yellow","orange","brown","pink"] )
+    a.mean(axis=1).plot(ax=ax, marker='s',figsize=(19, 9), markersize=5, linestyle='-', linewidth=3,color="black",label=ID_MT+"Avg")
+    b.plot(ax=ax1,marker='s',                  markersize=4, linestyle='--', linewidth=1,color=["blue","red","green","black","magenta","yellow","orange","brown","pink"] )
+    b.mean(axis=1).plot(ax=ax1, marker='s',figsize=(19, 9), markersize=5, linestyle='-', linewidth=3,color="black",label=ID_MT+"Avg")
+    c.plot(ax=ax2,marker='*',                  markersize=4, linestyle='--', linewidth=1,color=["blue","red","green","black","magenta","yellow","orange","brown","pink"] )
+    c.mean(axis=1).plot(ax=ax2,marker='s',figsize=(19, 9), markersize=5, linestyle='-', linewidth=3,color="black",label=ID_MT+"Avg")#               
+    
+    ax.set_ylabel('VH (dB)')
+    ax1.set_ylabel('VV (dB)')
+    ax2.set_ylabel('VH-VV')
+    ax.set(ylim=(-28, -12))
+    ax1.set(ylim=(-20, -7))
+    ax2.set(ylim=(-15, -2))
+        
+    ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+    ax1.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+    ax2.legend(loc='center left', bbox_to_anchor=(1.0, 0.5)) 
+    
+    ejes = fig.axes #which is used to extract the axes
+    # For every axis, set the x and y major locator
+    for axi in ejes:
+        axi.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=1, interval=2, tz=None))
+    plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right',)
+    ax2.tick_params(axis='x', which='major', labelsize=9)
+     
+    plt.suptitle(ID_MT,size=16)
+    plt.tight_layout()
+    fig.subplots_adjust(top=0.95)
+    
+    if save_ts =="Yes":
+        print("saving "+ID_MT)
+        Name=ID_MT
+        plt.savefig((out_path+Name),bbox_inches='tight')
+        
+    return(df1)
--- Earth_Engine_scripts/S1_time_Series.py
+++ Earth_Engine_scripts/S1_time_Series.py
@@ -1,146 +1,104 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug  4 01:03:05 2019
+Load the Sentinel-1 ImageCollection from the Earth engine
+Read shapefile and select Modulo/Turno/Lote to retrieve the time series
+Get the time series of a manually defined polygon
+Get the time series of a manually defined point (lat,lon)
 
-@author: crisj
+Pending:
+    Enable customization of plots (Change limits, titles, location of legends, dates x-axis ...)
+    Filter image collection by geometry (e.g. Doñana, Nazca, Agrisar, etc)
+    Combine incidence angles for each polarization
+    Visualice maps
+    
+@author: Cristian Silva
 """
 import ee
 ee.Initialize()
-import time
+#import time
 from datetime import datetime
-from pprint import pprint
-import geextract
-from geextract import ts_extract, get_date
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+#from pprint import pprint
+#import geextract
+#from geextract import ts_extract, get_date
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
 import geopandas
-
-#Parameters: DATES, DESCENDING OR DESCENDING, POLARISATION 
-str2='2019-08-04'
-str1='2016-12-01'
-#Direction='ASCENDING'
-Direction='DESCENDING'
-date2 = ee.Date(str2)
-date1 = ee.Date(str1)
-ds = date2.difference(date1, 'day')
-#---------------- Load the Sentinel-1 ImageCollection -----------------------
-sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD').filterDate(date1, date2).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH')).filter(ee.Filter.eq('instrumentMode', 'IW')).filter(ee.Filter.eq('orbitProperties_pass', Direction)).filter(ee.Filter.eq('relativeOrbitNumber_start', 142)); 
-
-lon= -78.79694540336544
-lat=-8.519651605063213
-geometry = ee.Geometry.Point(lon, lat)
-polygon = ee.Geometry.Polygon([[[-78.79737094642212,-8.520166448964439], 
-                                [-78.79594669343521,-8.520017902912258], 
-                                [-78.79635170699646,-8.519153151534463], 
-                                [-78.79773840905716,-8.519322918830348], 
-                                [-78.79737094642212,-8.520166448964439]]])
-
-Read_Sentinel_data = sentinel1.filterBounds(geometry).getRegion(geometry, 30).getInfo()
-out = geextract.dictify(Read_Sentinel_data)
-
-def get_S1_date(out):
-    """ Obtains the SAR image acquisition date from the product ID
-
-    Args:
-        out (List of dictionaries): Each dictionary corresponds to the sentinel1 info of an image
-
-    Returns:
-        Dates : A list of the acquisition dates
-    
-    """
-    Dates=np.zeros(len(out)).tolist()
-    VH=np.zeros(len(out)).tolist()
-    VV=np.zeros(len(out)).tolist()
-    angle=np.zeros(len(out)).tolist()
-    df=pd.DataFrame()
-    for i in range(len(out)):
-        a=out[i]['id']
-        b=a.split(sep='_')
-        b1=b[4].split(sep='T')
-        b2=b1[0]
-        Dates[i]=datetime.strptime(b2, "%Y%m%d")
-        VH[i]=out[i]['VH']
-        VV[i]=out[i]['VV']
-        angle[i]=out[i]['angle']
-    
-    
-    df['Dates']=Dates
-    df['VH']=VH
-    df['VV']=VV
-    df['angle']=angle
-    df.set_index('Dates', inplace=True)
-    
-    return(df)
-        
-df=get_S1_date(out)
-
-fig, (ax, ax1,) = plt.subplots(2, 1, sharex=True,figsize=(19,9)) 
-ax.plot(df['VH'])
-ax1.plot(df['VV'])
-
+import EE_library
+#import matplotlib.dates as mdates
+#---------------------------- Functions -----------------------------------------
 # Define function to map over imageCollection to perform spatial aggregation 
-def _reduce_region(image):
+#def _reduce_region(image,geometria):
     #if radius is not None or feature is not None:
     # Define spatial aggregation function
-    if stats == 'mean':
-        fun = ee.Reducer.mean()
-    elif stats == 'median':
-        fun = ee.Reducer.median()
-    elif stats == 'max':
-        fun = ee.Reducer.max()
-    elif stats == 'min':
-        fun = ee.Reducer.min()
-    elif stats == 'minMax':
-        fun = ee.Reducer.minMax()
-    else:
-        raise ValueError('Unknown spatial aggregation function. Must be one of mean, median, max, or min')
+#--------------------------------------------------------------------------------------------    
+save_ts = "No"
+path_out_img = ""
+#----------------------------Parameters: DATES, DESCENDING OR DESCENDING, POLARISATION------- 
+str2='2019-08-05'
+str1='2016-12-01'
+Direction='ASCENDING'
+#Direction='DESCENDING'
+#date2 = ee.Date(str2)
+date2=ee.Date(datetime.today())
+date1 = ee.Date(str1)
+ds = date2.difference(date1, 'day')
+orbit_No=91
+#---------------------------------- Load the Sentinel-1 ImageCollection -----------------------
+sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD').filterDate(date1, date2).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH')).filter(ee.Filter.eq('instrumentMode', 'IW')).filter(ee.Filter.eq('orbitProperties_pass', Direction)).filter(ee.Filter.eq('relativeOrbitNumber_start', orbit_No)); 
 
-    """Spatial aggregation function for a single image and a polygon feature"""
-    stat_dict = image.reduceRegion(fun, geometry, 30);
-    # FEature needs to be rebuilt because the backend doesn't accept to map
-    # functions that return dictionaries
-    return ee.Feature(None, stat_dict)
+#------------------------------------ Read shapefile and select Modulo/Turno/Lote to retrieve the time series ------------
+geo_df = geopandas.read_file("D:\\PhD Info\\EO4 Cultivar\\Ground data\\20180504_Danper_Ground_data\Shapefiles\\Danper_Campositan_parcels_boundaries_lotes_EPSG 4326 - WGS 84.shp")
 
-stats="mean"
-fc = sentinel1.filterBounds(polygon).map(_reduce_region).getInfo()
-out1 = geextract.simplify(fc)
+List_of_interesting_modulos = ["M19","M11","M24","M10","M23","M06","M22","M12","M14",'M07']
+List_of_interesting_turnos =  ["T04","T01","T02","T05","T05","T02","T03","T02","T05",'T04'] # Turno representativo de ese modulo
+filter_geo_df_by="ID_MT"
+stats = 'mean'    #   median, max, min, minMax
+out_path=""
+# If it was not Peru but a generic case, filter the geodataframe by the column desired and use ID_MT to locate the desired geometry
+for o in range(len(List_of_interesting_modulos)):
+    #ID_MT="M23T05"
+    ID_MT=List_of_interesting_modulos[o]+List_of_interesting_turnos[o]
+    geo_df_IDMT=geo_df.loc[geo_df[filter_geo_df_by] == ID_MT]                   
+    df_shp=EE_library.time_series_from_shp(sentinel1,geo_df_IDMT,ID_MT,stats,save_ts,out_path)
 
-df1=get_S1_date(out1)
-df1.sort_index()
-
-plt.style.use('ggplot')
-fig, (ax, ax1,) = plt.subplots(2, 1, sharex=True,figsize=(19,9)) 
-df['VH'].plot(ax=ax,marker='o', markersize=4, linestyle='--', linewidth=1,color="blue",label="Point")
-df1['VH'].plot(ax=ax1,marker='o', markersize=4, linestyle='--', linewidth=1,color="red",label="polygon")
-ax.legend()
-ax1.legend()
-plt.tight_layout()
-
-#print(polygon.coordinates())
-
+#-------------------------------------Define manually the point/polygon to get time series for ----------------------------------------------------------
+#Geometries for test
 M06T01L237 = ee.Geometry.Polygon([[[-78.81792738406443, -8.491412672228353],
                                    [-78.81786032883906, -8.492288097425805],
                                    [-78.81688668696665, -8.492158110174252],
                                    [-78.8170020219543, -8.491298601525253]]])
 
-fc1 = sentinel1.filterBounds(M06T01L237).map(_reduce_region).getInfo()
-out2 = geextract.simplify(fc1)
+M24T03L085 = ee.Geometry.Polygon([[[-78.79227799971744, -8.532604673332454],
+                                   [-78.79161281188175, -8.533113957068709],
+                                   [-78.7907759626691, -8.532052948518402],
+                                   [-78.79147333701297, -8.531554273481474]]])
+                                   
+M07T04L020 = ee.Geometry.Polygon([[[-78.82064779049574, -8.471746481621691],
+                                   [-78.82111449486433, -8.472574198772755],
+                                   [-78.82030446774183, -8.47303050362005],
+                                   [-78.81983239895521, -8.47214442278201]]])  
 
-df2=get_S1_date(out2)
-df2.sort_index
+Parcela_A_Doñana=ee.Geometry.Polygon([[     [-6.109310154489094,37.05954307806896 ],
+                                            [-6.100791458657795,37.0607417086733  ],
+                                            [-6.102529530099446,37.06158073882878 ],
+                                            [-6.110297207406575,37.060536230486036],
+                                            [-6.109310154489094,37.05954307806896 ]]])                               
 
-fig, ax = plt.subplots(1, 1, sharex=True,figsize=(19,9)) 
-#df1['VH'].plot(ax=ax,marker='o', markersize=4, linestyle='--', linewidth=1,color="blue",label="M14T04L490")
-df2['VH'].plot(ax=ax,marker='o', markersize=4, linestyle='--', linewidth=1,color="red",label="M06T01L237")
-ax.legend()
-#ax1.legend()
-plt.tight_layout()
+geometria=['M06T01L237','M24T03L085','M07T04L020'] # Iterate over this list of geometries in case more than one region time series is needed
+#---------------------------------------- Time series of a region (image.reduceRegion) -------------------
+#sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD').filterDate(date1, date2).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')).filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH')).filter(ee.Filter.eq('instrumentMode', 'IW')).filter(ee.Filter.eq('orbitProperties_pass', Direction)); 
+geometry=Parcela_A_Doñana #M06T01L237    #  
+#sentinel1=sentinel1.filterBounds(geometry)
+stats = 'mean'    #   median, max, min, minMax
+title='M06T01L237'
+df_region=EE_library.Time_series_of_a_region(sentinel1,geometry,stats,title)
+#---------------------------------------------------- Time series of a point -------------------------------------------
+lon= -78.79694540336544
+lat=-8.519651605063213
+point = ee.Geometry.Point(lon, lat)
+title='Point in M06T01L237'   
+df_point=EE_library.Time_series_of_a_point(sentinel1,point,title)
 
-geo_df = geopandas.read_file("D:\\PhD Info\\EO4 Cultivar\\Ground data\\20180504_Danper_Ground_data\Shapefiles\\Danper_Campositan_parcels_boundaries_lotes_EPSG 4326 - WGS 84.shp")
-a=geo_df['geometry'][0]
 
-b=str(a)
-c=b.split(sep='POLYGON Z ((')
-print(c[1])
 
